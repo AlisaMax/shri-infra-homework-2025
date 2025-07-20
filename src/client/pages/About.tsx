ef090b6d0108330c202011c07dd9faa287ba80ff
Daniel <116294062+VBNetEnjoyer@users.noreply.github.com>
2025-07-20 13:09:14 +0300

    Revert "Поломал"
    This reverts commit 3d325d8548ef2a2825813b3209fc7c0e5445cb38.

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -5,15 +5,15 @@ import { cn } from "@bem-react/classname";
 const bem = cn("About");
 
 export const About: React.FC = () => {
-	return (
-		<div className={bem()}>
-			<Helmet title="About" />
-			<div className="row">
-				<div className="col">
-					<h1>About</h1>
-					<p>Author: cccccca</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={bem()}>
+      <Helmet title="About" />
+      <div className="row">
+        <div className="col">
+          <h1>About</h1>
+          <p>Author: Alisa Maximenkova</p>
+        </div>
+      </div>
+    </div>
+  );
 };
